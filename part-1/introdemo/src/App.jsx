a0626a7b23acba4b6598d7badd8aacc64afe4b3a
Jaime Pineda <jpin730@pm.me>
2025-07-25 18:23:34 -0600

    feat: :sparkles: implement Hello component and update App to display friends' greetings

--- part-1/introdemo/src/App.jsx
+++ part-1/introdemo/src/App.jsx
@@ -1,7 +1,24 @@
-const App = () => {
+const Hello = (props) => {
   return (
     <div>
-      <p>Hello World!</p>
+      <p>
+        Hello {props.name}, you are {props.age} years old
+      </p>
+    </div>
+  )
+}
+
+const App = () => {
+  const friends = [
+    { id: crypto.randomUUID(), name: 'Peter', age: 4 },
+    { id: crypto.randomUUID(), name: 'Maya', age: 10 },
+  ]
+  return (
+    <div>
+      <h1>Greetings</h1>
+      {friends.map((friend) => (
+        <Hello key={friend.id} name={friend.name} age={friend.age} />
+      ))}
     </div>
   )
 }
