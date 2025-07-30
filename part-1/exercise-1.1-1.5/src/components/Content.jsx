f11d9e639f31342631aa9264cd85f9c71e2b546e
Jaime Pineda <jpin730@pm.me>
2025-07-30 14:21:13 -0600

    refactor: :recycle: consolidate part data into a single array for improved readability and maintainability

--- part-1/exercise-1.1-1.5/src/components/Content.jsx
+++ part-1/exercise-1.1-1.5/src/components/Content.jsx
@@ -3,9 +3,9 @@ import Part from './Part'
 const Content = (props) => {
   return (
     <>
-      <Part part={props.part1} exercises={props.exercises1} />
-      <Part part={props.part2} exercises={props.exercises2} />
-      <Part part={props.part3} exercises={props.exercises3} />
+      <Part part={props.parts.at(0).name} exercises={props.parts.at(0).exercises} />
+      <Part part={props.parts.at(1).name} exercises={props.parts.at(1).exercises} />
+      <Part part={props.parts.at(2).name} exercises={props.parts.at(2).exercises} />
     </>
   )
 }
--- part-1/exercise-1.1-1.5/src/components/Total.jsx
+++ part-1/exercise-1.1-1.5/src/components/Total.jsx
@@ -1,5 +1,10 @@
 const Total = (props) => {
-  return <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+  return (
+    <p>
+      Number of exercises{' '}
+      {props.parts.at(0).exercises + props.parts.at(1).exercises + props.parts.at(2).exercises}
+    </p>
+  )
 }
 
 export default Total
