21ebb48f0aa81525fccc0963055756b11c5de6bd
Jaime Pineda <jpin730@pm.me>
2025-08-29 10:11:03 -0600

    feat: :sparkles: create StatisticLine component for displaying statistics

---
+++ part-1/exercise-1.6-1.14/src/components/StatisticLine.jsx
@@ -0,0 +1,9 @@
+export const StatisticLine = ({ text, value }) => {
+  return (
+    <p>
+      {text}: {value}
+    </p>
+  )
+}
+
+export default StatisticLine
--- part-1/exercise-1.6-1.14/src/components/Statistics.jsx
+++ part-1/exercise-1.6-1.14/src/components/Statistics.jsx
@@ -1,3 +1,5 @@
+import StatisticLine from './StatisticLine'
+
 const Statistics = ({ good, neutral, bad }) => {
   const feedbackCount = good + neutral + bad
   const average = feedbackCount === 0 ? 0 : (good - bad) / feedbackCount
@@ -8,12 +10,15 @@ const Statistics = ({ good, neutral, bad }) => {
       <h2>Statistics</h2>
       {feedbackCount > 0 ? (
         <>
-          <p>Good: {good}</p>
-          <p>Neutral: {neutral}</p>
-          <p>Bad: {bad}</p>
-          <p>All: {feedbackCount}</p>
-          <p>Average: {average.toFixed(2)}</p>
-          <p>Positive feedback: {positiveFeedbackPercentage.toFixed(2)}%</p>
+          <StatisticLine text="Good" value={good} />
+          <StatisticLine text="Neutral" value={neutral} />
+          <StatisticLine text="Bad" value={bad} />
+          <StatisticLine text="All" value={feedbackCount} />
+          <StatisticLine text="Average" value={average.toFixed(2)} />
+          <StatisticLine
+            text="Positive feedback"
+            value={`${positiveFeedbackPercentage.toFixed(2)}%`}
+          />
         </>
       ) : (
         <p>No feedback given</p>
