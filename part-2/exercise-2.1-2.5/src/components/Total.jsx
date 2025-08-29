b05ea2fded1e6e4ec44a3d4932106f3012926f32
Jaime Pineda <jpin730@pm.me>
2025-08-29 17:59:47 -0600

    feat: :sparkles: add Redux part to course and calculate total exercises in Total component

--- part-2/exercise-2.1-2.5/src/components/Total.jsx
+++ part-2/exercise-2.1-2.5/src/components/Total.jsx
@@ -1,5 +1,10 @@
-const Total = () => {
-  return <p>Number of exercises</p>
+const Total = ({ parts }) => {
+  const total = parts.reduce((sum, part) => sum + part.exercises, 0)
+  return (
+    <p>
+      <strong>Number of exercises {total}</strong>
+    </p>
+  )
 }
 
 export default Total
