5b7474d8bc432ff70c36e230b0fa8d19d4a90a17
Jaime Pineda <jpin730@pm.me>
2025-09-07 23:55:56 -0600

    feat: :tada: refactor addPerson to utilize updatePerson for existing entries

--- part-2/exercise-2.6-2.17/src/App.jsx
+++ part-2/exercise-2.6-2.17/src/App.jsx
@@ -23,12 +23,26 @@ const App = () => {
 
   const onChangeFilter = (value) => setFilter(value)
 
-  const addPerson = async ({ name, number }) => {
-    const nameExists = name in persons
+  const updatePerson = async ({ id, name, number }) => {
+    const confirmMessage = `${name} is already added to phone book, replace the old number with a new one?`
+    const confirmed = confirm(confirmMessage)
+    if (!confirmed) return false
 
-    if (nameExists) {
-      alert(`${name} is already added to phone book`)
+    try {
+      const updatedPerson = await personsService.update(id, { name, number })
+      setPersons({ ...persons, [updatedPerson.name]: updatedPerson })
+      return true
+    } catch (error) {
+      console.error('Error updating person:', error)
       return false
+    }
+  }
+
+  const addPerson = async ({ name, number }) => {
+    const foundPerson = persons[name]
+
+    if (foundPerson) {
+      return updatePerson({ ...foundPerson, number })
     }
 
     try {
