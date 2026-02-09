285582b19ac2b4b1b4f975364a06c7d4ea8fdb82
Jaime Pineda <jpin730@pm.me>
2026-02-09 09:38:39 -0600

    feat: :sparkles: add weather information display in CountryDetail component

--- part-2/exercise-2.18-2.20/src/components/CountryDetail.jsx
+++ part-2/exercise-2.18-2.20/src/components/CountryDetail.jsx
@@ -1,4 +1,21 @@
+import { useEffect, useState } from 'react'
+import countriesService from '../services/countries'
+
 const CountryDetail = ({ country }) => {
+  const [weather, setWeather] = useState(null)
+
+  const capital = country.capital[0]
+  const openWeatherIconBaseUrl = 'https://openweathermap.org/payload/api/media/file'
+
+  useEffect(() => {
+    const abortController = new AbortController()
+    countriesService
+      .getWeatherByCapital(capital, abortController.signal)
+      .then((data) => setWeather(data))
+
+    return () => abortController.abort()
+  }, [capital])
+
   return (
     <div>
       <h2>{country.name.common}</h2>
@@ -11,6 +28,17 @@ const CountryDetail = ({ country }) => {
         ))}
       </ul>
       <img src={country.flags.png} alt={`Flag of ${country.name.common}`} width="200" />
+      {weather && (
+        <>
+          <h3>Weather in {capital}</h3>
+          <p>Temperature {weather.main.temp} °C</p>
+          <img
+            src={`${openWeatherIconBaseUrl}/${weather.weather[0].icon}.png`}
+            alt={weather.weather[0].description}
+          />
+          <p>Wind {weather.wind.speed} m/s</p>
+        </>
+      )}
     </div>
   )
 }
