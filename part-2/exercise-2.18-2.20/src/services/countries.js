1e26af6987eb0b2e49c261cf7fa1d6a08e5f1217
Jaime Pineda <jpin730@pm.me>
2026-02-09 09:37:12 -0600

    feat: :sparkles: add getWeatherByCapital function to fetch weather data by capital

--- part-2/exercise-2.18-2.20/src/services/countries.js
+++ part-2/exercise-2.18-2.20/src/services/countries.js
@@ -1,6 +1,18 @@
+const restCountriesBaseUrl = 'https://studies.cs.helsinki.fi/restcountries/api'
+const openWeatherBaseUrl = 'https://api.openweathermap.org/data/2.5'
+const openWeatherApiKey = import.meta.env.VITE_OPEN_WEATHER_API_KEY
+
 const getAll = () => {
-  const url = 'https://studies.cs.helsinki.fi/restcountries/api/all'
+  const url = new URL(`${restCountriesBaseUrl}/all`)
   return fetch(url).then((response) => response.json())
 }
 
-export default { getAll }
+const getWeatherByCapital = (capital, signal) => {
+  const url = new URL(`${openWeatherBaseUrl}/weather`)
+  url.searchParams.append('q', capital)
+  url.searchParams.append('appid', openWeatherApiKey)
+  url.searchParams.append('units', 'metric')
+  return fetch(url, { signal }).then((response) => response.json())
+}
+
+export default { getAll, getWeatherByCapital }
