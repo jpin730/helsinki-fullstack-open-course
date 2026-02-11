3852f782c4a797382258d97cedcd249615546018
Jaime Pineda <jpin730@pm.me>
2026-02-11 15:18:03 -0600

    feat: :sparkles: add API endpoint to retrieve persons data

--- part-3/exercise-3.1-3.11/index.js
+++ part-3/exercise-3.1-3.11/index.js
@@ -1,8 +1,35 @@
 const express = require('express')
 const app = express()
 
+const PERSONS = [
+  {
+    id: '1',
+    name: 'Arto Hellas',
+    number: '040-123456'
+  },
+  {
+    id: '2',
+    name: 'Ada Lovelace',
+    number: '39-44-5323523'
+  },
+  {
+    id: '3',
+    name: 'Dan Abramov',
+    number: '12-43-234345'
+  },
+  {
+    id: '4',
+    name: 'Mary Poppendieck',
+    number: '39-23-6423122'
+  }
+]
+
 app.get('/', (_, response) => {
   response.send('Hello  World!')
+})
+
+app.get('/api/persons', (_, response) => {
+  response.json(PERSONS)
 })
 
 const PORT = 3001
