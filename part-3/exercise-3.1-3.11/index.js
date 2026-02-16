d631da967e9ef6e29acda4942a24093244aac112
Jaime Pineda <jpin730@pm.me>
2026-02-16 00:32:09 -0600

    refactor: :fire: comment out manual CORS middleware in index.js

--- part-3/exercise-3.1-3.11/index.js
+++ part-3/exercise-3.1-3.11/index.js
@@ -9,18 +9,18 @@ const app = express()
 app.use(express.json())
 
 // Manual CORS middleware
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*')
+//   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+//   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
   
-  // Handle preflight requests
-  if (req.method === 'OPTIONS') {
-    return res.sendStatus(200)
-  }
+//   // Handle preflight requests
+//   if (req.method === 'OPTIONS') {
+//     return res.sendStatus(200)
+//   }
   
-  next()
-})
+//   next()
+// })
 
 app.use(express.static('public'))
 
