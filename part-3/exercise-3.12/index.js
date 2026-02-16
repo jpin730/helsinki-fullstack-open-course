600de266c3acec9599ab266dbf19fe4cdaad80f1
Jaime Pineda <jpin730@pm.me>
2026-02-16 15:14:58 -0600

    feat: :sparkles: add MongoDB URI handling and environment variable check

--- part-3/exercise-3.12/index.js
+++ part-3/exercise-3.12/index.js
@@ -1,3 +1,11 @@
+process.loadEnvFile()
+let mongoDbUri = process.env.MONGODB_URI
+
+if (!mongoDbUri) {
+  console.log('Please provide the MongoDB URI as an environment variable: MONGODB_URI')
+  process.exit(1)
+}
+
 const args = process.argv
 
 if (args.length < 3) {
@@ -8,6 +16,9 @@ if (args.length < 3) {
 const password = args[2]
 const personName = args[3]
 const personNumber = args[4]
+
+mongoDbUri = mongoDbUri.replace('<db_password>', password)
+console.log(mongoDbUri)
 
 if (personName && !personNumber) {
   console.log('Please provide the number as an argument: node index.js <password> <name> <number>')
