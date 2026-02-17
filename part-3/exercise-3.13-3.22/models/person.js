726ebc2abe8c18726513a901e1c3574d39d7ace1
Jaime Pineda <jpin730@pm.me>
2026-02-17 15:03:41 -0600

    feat: :safety_vest: enhance person schema with type, minLength, and required validations

--- part-3/exercise-3.13-3.22/models/person.js
+++ part-3/exercise-3.13-3.22/models/person.js
@@ -9,8 +9,16 @@ mongoose.connect(url, { family: 4 })
   .catch(error => console.log('Error connecting to MongoDB:', error.message))
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String
+  name: {
+    type: String,
+    minLength: 3,
+    required: true
+  },
+  number: {
+    type: String,
+    minLength: 3,
+    required: true
+  }
 })
 
 personSchema.set('toJSON', {
