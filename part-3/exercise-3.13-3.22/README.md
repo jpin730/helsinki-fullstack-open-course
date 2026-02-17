7814f251f9ccbe0a7a6ec749cce93bd13980e49b
Jaime Pineda <jpin730@pm.me>
2026-02-16 22:56:44 -0600

    feat: :tada: initial commit exercise-3.13-3.22

---
+++ part-3/exercise-3.13-3.22/README.md
@@ -0,0 +1,47 @@
+# Exercise 3.13-3.22
+
+A simple phonebook backend API built with Express.js.
+
+This app was created using the command:
+
+```bash
+npm init
+```
+
+## How to Run the App
+
+To run this app, follow these steps:
+
+Install the dependencies:
+
+```bash
+npm install
+```
+
+Start the development server with auto-reload:
+
+```bash
+npm run dev
+```
+
+Also, you can start the server in production mode without auto-reload:
+
+```bash
+npm start
+```
+
+If you want to debug the application using Visual Studio Code, just press F5 or go to the Run option menu and select "Start Debugging".
+
+## Testing with REST Client
+
+HTTP requests are available in the `requests/` folder. Install the [REST Client](https://marketplace.visualstudio.com/items?itemName=humao.rest-client) VS Code extension to use them.
+
+The base URL is configured in [.vscode/settings.json](.vscode/settings.json) for the `local` environment.
+
+Production environment requests are available by selecting the `production` environment in the REST Client.
+
+## Deployment
+
+The app is deployed at: <https://helsinki-fullstack-open-course.onrender.com>
+
+> NOTE: Before running the app, make sure to run the `npm run build:ui` script to copy the built frontend files from the previous part of the course.
---
+++ part-3/exercise-3.13-3.22/index.js
@@ -0,0 +1,96 @@
+const express = require('express')
+const morgan = require('morgan')
+
+const HTTP_STATUS = require('./consts/http-status')
+const PERSONS = require('./consts/persons')
+
+const app = express()
+
+app.use(express.json())
+
+// Manual CORS middleware
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*')
+//   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+//   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+  
+//   // Handle preflight requests
+//   if (req.method === 'OPTIONS') {
+//     return res.sendStatus(200)
+//   }
+  
+//   next()
+// })
+
+app.use(express.static('public'))
+
+morgan.token('body', (req) =>
+  req.body && Object.keys(req.body).length > 0 ? JSON.stringify(req.body) : ''
+)
+
+app.use(
+  morgan(':method :url :status :res[content-length] - :response-time ms :body')
+)
+
+app.get('/api', (_, response) => {
+  response.send('Hello  World!')
+})
+
+app.get('/api/info', (_, response) => {
+  const date = new Date()
+  response.send(
+    `<p>Phonebook has info for ${PERSONS.length} people</p><p>${date}</p>`
+  )
+})
+
+app.get('/api/persons', (_, response) => {
+  response.json(PERSONS)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  const person = PERSONS.find((p) => p.id === id)
+
+  if (person == null) {
+    return response.status(HTTP_STATUS.NOT_FOUND).end()
+  }
+
+  response.json(person)
+})
+
+app.post('/api/persons', (request, response) => {
+  const { name, number } = request.body
+
+  if (name == null || number == null) {
+    return response.status().json({ error: 'name or number is missing' })
+  }
+
+  const nameExists = PERSONS.some((p) => p.name === name)
+
+  if (nameExists) {
+    return response
+      .status(HTTP_STATUS.BAD_REQUEST)
+      .json({ error: 'name must be unique' })
+  }
+
+  const id = (Math.random() * 1000000).toFixed(0)
+  const newPerson = { id, name, number }
+  PERSONS.push(newPerson)
+
+  response.status(HTTP_STATUS.CREATED).json(newPerson)
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  const personIndex = PERSONS.findIndex((p) => p.id === id)
+
+  if (personIndex === -1) {
+    return response.status(HTTP_STATUS.NOT_FOUND).end()
+  }
+
+  PERSONS.splice(personIndex, 1)
+  response.status(HTTP_STATUS.NO_CONTENT).end()
+})
+
+const PORT = process.env.PORT ?? 3001
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
