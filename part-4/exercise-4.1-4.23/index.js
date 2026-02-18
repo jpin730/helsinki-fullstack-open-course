9ccd4a30db7a258e05d02fd4bdb5c2bf5d2b30e0
Jaime Pineda <jpin730@pm.me>
2026-02-18 12:11:38 -0600

    feat: :sparkles: implement creation and reading of posts

--- part-4/exercise-4.1-4.23/index.js
+++ part-4/exercise-4.1-4.23/index.js
@@ -1,2 +1,42 @@
-// eslint-disable-next-line no-console
-console.log('Hello world!');
+const express = require('express');
+const mongoose = require('mongoose');
+
+const HTTP_STATUS = require('./consts/http-status');
+
+process.loadEnvFile();
+
+const app = express();
+
+const blogSchema = mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number,
+});
+
+const Blog = mongoose.model('Blog', blogSchema);
+
+const mongoUrl = process.env.MONGODB_URI;
+mongoose.connect(mongoUrl, { family: 4 }).then(() =>
+  // TODO: Remove console.log and use a logger instead
+  // eslint-disable-next-line no-console
+  console.log('Connected to MongoDB'),
+);
+
+app.use(express.json());
+
+app.get('/api/blogs', (_, response) => {
+  Blog.find({}).then((blogs) => response.json(blogs));
+});
+
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body);
+  blog.save().then((result) => response.status(HTTP_STATUS.CREATED).json(result));
+});
+
+const PORT = process.env.PORT;
+app.listen(PORT, () =>
+  // TODO: Remove console.log and use a logger instead
+  // eslint-disable-next-line no-console
+  console.log(`Server running on port ${PORT}`),
+);
