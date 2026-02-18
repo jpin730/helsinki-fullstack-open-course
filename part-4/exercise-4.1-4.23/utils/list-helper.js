2061b3c62fdaeddef18ba1f8ad9c61aeb5a66f63
Jaime Pineda <jpin730@pm.me>
2026-02-18 15:33:24 -0600

    test: :white_check_mark: add and test favoriteBlog function in list helper

--- part-4/exercise-4.1-4.23/utils/list-helper.js
+++ part-4/exercise-4.1-4.23/utils/list-helper.js
@@ -2,7 +2,16 @@ const dummy = () => 1;
 
 const totalLikes = (blogs = []) => blogs.reduce((sum, blog) => sum + blog.likes, 0);
 
+const favoriteBlog = (blogs = []) => {
+  if (blogs.length === 0) return null;
+
+  const favorite = blogs.reduce((prev, current) => (current.likes > prev.likes ? current : prev));
+
+  return structuredClone(favorite);
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
