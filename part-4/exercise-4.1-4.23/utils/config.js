1c6e57f1de409354df0d890e36590a260f4240b4
Jaime Pineda <jpin730@pm.me>
2026-02-18 17:23:25 -0600

    feat: :wrench: add NODE_ENV to config with cross-env support

--- part-4/exercise-4.1-4.23/utils/config.js
+++ part-4/exercise-4.1-4.23/utils/config.js
@@ -1,6 +1,7 @@
 process.loadEnvFile();
 
-const PORT = process.env.PORT;
 const MONGODB_URI = process.env.MONGODB_URI;
+const NODE_ENV = process.env.NODE_ENV;
+const PORT = process.env.PORT;
 
-module.exports = { MONGODB_URI, PORT };
+module.exports = { MONGODB_URI, NODE_ENV, PORT };
