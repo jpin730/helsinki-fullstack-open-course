0e135a9e649c4740b0d114909135aca7ad974f39
Jaime Pineda <jpin730@pm.me>
2026-02-18 11:34:44 -0600

    chore: :wrench: add eslint, prettier, husky and lint-staged config

---
+++ part-4/exercise-4.1-4.23/eslint.config.mjs
@@ -0,0 +1,37 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import json from '@eslint/json';
+import markdown from '@eslint/markdown';
+import { defineConfig } from 'eslint/config';
+import prettier from 'eslint-plugin-prettier/recommended';
+
+export default defineConfig([
+  prettier,
+  { ignores: ['package-lock.json'] },
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+    languageOptions: { globals: globals.node },
+  },
+  {
+    files: ['**/*.js'],
+    languageOptions: { sourceType: 'commonjs' },
+    rules: {
+      'no-console': 'error',
+    },
+  },
+  { files: ['**/*.json'], plugins: { json }, language: 'json/json', extends: ['json/recommended'] },
+  {
+    files: ['**/*.jsonc'],
+    plugins: { json },
+    language: 'json/jsonc',
+    extends: ['json/recommended'],
+  },
+  {
+    files: ['**/*.md'],
+    plugins: { markdown },
+    language: 'markdown/commonmark',
+    extends: ['markdown/recommended'],
+  },
+]);
