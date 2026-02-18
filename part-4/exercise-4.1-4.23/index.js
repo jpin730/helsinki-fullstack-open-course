c87a4664282de34f27749dfd9ba6cf7e3ffe7400
Jaime Pineda <jpin730@pm.me>
2026-02-18 12:36:19 -0600

    refactor: :recycle: implemente app module for initialization

--- part-4/exercise-4.1-4.23/index.js
+++ part-4/exercise-4.1-4.23/index.js
@@ -1,22 +1,5 @@
-const express = require('express');
-const mongoose = require('mongoose');
-
+const app = require('./app');
 const config = require('./utils/config');
 const logger = require('./utils/logger');
 
-const blogsRouter = require('./controllers/blogs');
-
-const app = express();
-
-mongoose
-  .connect(config.MONGODB_URI, { family: 4 })
-  .then(() => logger.info('Connected to MongoDB'))
-  .catch((error) => logger.error('Error connection to MongoDB:', error.message));
-
-app.use(express.json());
-
-app.use('/api/blogs', blogsRouter);
-
-const PORT = config.PORT;
-
-app.listen(PORT, () => logger.info(`Server running on port ${PORT}`));
+app.listen(config.PORT, () => logger.info(`Server running on port ${config.PORT}`));
