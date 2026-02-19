309dc4e0b7d11911fbaac5be632af04a26eeb980
Jaime Pineda <jpin730@pm.me>
2026-02-19 12:26:04 -0600

    refactor: :safety_vest: enforce required fields for title and url in blog schema

--- part-4/exercise-4.1-4.23/models/blog.js
+++ part-4/exercise-4.1-4.23/models/blog.js
@@ -3,9 +3,9 @@ const mongoose = require('mongoose');
 const config = require('../utils/config');
 
 const blogSchema = mongoose.Schema({
-  title: String,
+  title: { type: String, required: true },
   author: String,
-  url: String,
+  url: { type: String, required: true },
   likes: { type: Number, default: 0 },
 });
 
