6d6c0010c3bac69fcd1b1578fbb8c87dc8e978f4
Jaime Pineda <jpin730@pm.me>
2026-02-19 12:01:12 -0600

    test: :white_check_mark: add test to verify correct amount of listed blog

--- part-4/exercise-4.1-4.23/tests/blog-api.test.js
+++ part-4/exercise-4.1-4.23/tests/blog-api.test.js
@@ -1,17 +1,18 @@
 const { after, beforeEach, test } = require('node:test');
+const assert = require('node:assert');
 const mongoose = require('mongoose');
 const supertest = require('supertest');
 
 const app = require('../app');
 const HTTP_STATUS = require('../consts/http-status');
 const Blog = require('../models/blog');
-const { BLOGS } = require('./test-helper');
+const helper = require('./test-helper');
 
 const api = supertest(app);
 
 beforeEach(async () => {
   await Blog.deleteMany({});
-  await Blog.insertMany(BLOGS);
+  await Blog.insertMany(helper.BLOGS);
 });
 
 test('blogs are returned as json', async () => {
@@ -19,6 +20,12 @@ test('blogs are returned as json', async () => {
     .get('/api/blogs')
     .expect(HTTP_STATUS.OK)
     .expect('Content-Type', /application\/json/);
+});
+
+test('all blogs are returned', async () => {
+  const response = await api.get('/api/blogs');
+
+  assert.strictEqual(response.body.length, helper.BLOGS.length);
 });
 
 after(async () => {
