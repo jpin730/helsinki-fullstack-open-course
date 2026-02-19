f63295a9e70bc33dd54a9895bb4130fc6130a009
Jaime Pineda <jpin730@pm.me>
2026-02-19 12:55:59 -0600

    test: :white_check_mark: restructure blog API tests for better organization and clarity

--- part-4/exercise-4.1-4.23/tests/blog-api.test.js
+++ part-4/exercise-4.1-4.23/tests/blog-api.test.js
@@ -1,4 +1,4 @@
-const { after, beforeEach, test } = require('node:test')
+const { after, beforeEach, describe, test } = require('node:test')
 const assert = require('node:assert')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
@@ -15,92 +15,96 @@ beforeEach(async () => {
   await Blog.insertMany(helper.BLOGS)
 })
 
-test('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(HTTP_STATUS.OK)
-    .expect('Content-Type', /application\/json/)
+describe('GET /api/blogs', () => {
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(HTTP_STATUS.OK)
+      .expect('Content-Type', /application\/json/)
+  })
+
+  test('all blogs are returned', async () => {
+    const response = await api.get('/api/blogs')
+
+    assert.strictEqual(response.body.length, helper.BLOGS.length)
+  })
+
+  test('unique identifier of blogs is named id', async () => {
+    const response = await api.get('/api/blogs')
+
+    const blog = response.body.at(0)
+    assert.ok(blog.id)
+    assert.strictEqual(blog._id, undefined)
+  })
 })
 
-test('all blogs are returned', async () => {
-  const response = await api.get('/api/blogs')
+describe('POST /api/blogs', () => {
+  test('a valid blog can be added', async () => {
+    const newBlog = {
+      title: 'Test blog',
+      author: 'Test author',
+      url: 'https://test.com',
+      likes: 0,
+    }
 
-  assert.strictEqual(response.body.length, helper.BLOGS.length)
-})
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(HTTP_STATUS.CREATED)
+      .expect('Content-Type', /application\/json/)
 
-test('unique identifier of blogs is named id', async () => {
-  const response = await api.get('/api/blogs')
+    const blogsInDb = await helper.getBlogsInDb()
+    assert.strictEqual(blogsInDb.length, helper.BLOGS.length + 1)
 
-  const blog = response.body.at(0)
-  assert.ok(blog.id)
-  assert.strictEqual(blog._id, undefined)
-})
+    const titles = blogsInDb.map((blog) => blog.title)
+    assert(titles.includes(newBlog.title))
+  })
 
-test('a valid blog can be added', async () => {
-  const newBlog = {
-    title: 'Test blog',
-    author: 'Test author',
-    url: 'https://test.com',
-    likes: 0,
-  }
+  test('blog without likes defaults to 0', async () => {
+    const newBlog = {
+      title: 'Test blog without likes',
+      author: 'Test author',
+      url: 'https://test.com',
+    }
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(HTTP_STATUS.CREATED)
-    .expect('Content-Type', /application\/json/)
+    const response = await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(HTTP_STATUS.CREATED)
+      .expect('Content-Type', /application\/json/)
 
-  const blogsInDb = await helper.getBlogsInDb()
-  assert.strictEqual(blogsInDb.length, helper.BLOGS.length + 1)
+    assert.strictEqual(response.body.likes, 0)
 
-  const titles = blogsInDb.map((blog) => blog.title)
-  assert(titles.includes(newBlog.title))
-})
+    const blogsInDb = await helper.getBlogsInDb()
+    const addedBlog = blogsInDb.find((blog) => blog.id === response.body.id)
+    assert.strictEqual(addedBlog.likes, 0)
+  })
 
-test('blog without likes defaults to 0', async () => {
-  const newBlog = {
-    title: 'Test blog without likes',
-    author: 'Test author',
-    url: 'https://test.com',
-  }
+  test('blog without title is not added', async () => {
+    const newBlog = {
+      author: 'Test author',
+      url: 'https://test.com',
+      likes: 0,
+    }
 
-  const response = await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(HTTP_STATUS.CREATED)
-    .expect('Content-Type', /application\/json/)
+    await api.post('/api/blogs').send(newBlog).expect(HTTP_STATUS.BAD_REQUEST)
 
-  assert.strictEqual(response.body.likes, 0)
+    const blogsInDb = await helper.getBlogsInDb()
+    assert.strictEqual(blogsInDb.length, helper.BLOGS.length)
+  })
 
-  const blogsInDb = await helper.getBlogsInDb()
-  const addedBlog = blogsInDb.find((blog) => blog.id === response.body.id)
-  assert.strictEqual(addedBlog.likes, 0)
-})
+  test('blog without url is not added', async () => {
+    const newBlog = {
+      title: 'Test blog',
+      author: 'Test author',
+      likes: 0,
+    }
 
-test('blog without title is not added', async () => {
-  const newBlog = {
-    author: 'Test author',
-    url: 'https://test.com',
-    likes: 0,
-  }
+    await api.post('/api/blogs').send(newBlog).expect(HTTP_STATUS.BAD_REQUEST)
 
-  await api.post('/api/blogs').send(newBlog).expect(HTTP_STATUS.BAD_REQUEST)
-
-  const blogsInDb = await helper.getBlogsInDb()
-  assert.strictEqual(blogsInDb.length, helper.BLOGS.length)
-})
-
-test('blog without url is not added', async () => {
-  const newBlog = {
-    title: 'Test blog',
-    author: 'Test author',
-    likes: 0,
-  }
-
-  await api.post('/api/blogs').send(newBlog).expect(HTTP_STATUS.BAD_REQUEST)
-
-  const blogsInDb = await helper.getBlogsInDb()
-  assert.strictEqual(blogsInDb.length, helper.BLOGS.length)
+    const blogsInDb = await helper.getBlogsInDb()
+    assert.strictEqual(blogsInDb.length, helper.BLOGS.length)
+  })
 })
 
 after(async () => {
