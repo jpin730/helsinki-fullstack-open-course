ec14c2bdbe722009ee4e43f4ac8b63e54230fb48
Jaime Pineda <jpin730@pm.me>
2026-02-19 16:42:55 -0600

    feat: :sparkles: implement user registration endpoint with password validation and hashing

--- part-4/exercise-4.1-4.23/app.js
+++ part-4/exercise-4.1-4.23/app.js
@@ -6,6 +6,7 @@ const logger = require('./utils/logger')
 const middleware = require('./utils/middleware')
 
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 
 const app = express()
 
@@ -18,6 +19,7 @@ app.use(express.json())
 app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
---
+++ part-4/exercise-4.1-4.23/controllers/users.js
@@ -0,0 +1,35 @@
+const bcrypt = require('bcrypt')
+
+const usersRouter = require('express').Router()
+
+const HTTP_STATUS = require('../consts/http-status')
+const User = require('../models/user')
+
+usersRouter.post('/', async (request, response) => {
+  const { username, name, password } = request.body
+
+  if (!password || password.length < 3) {
+    return response
+      .status(HTTP_STATUS.BAD_REQUEST)
+      .json({ error: 'password must be at least 3 characters long' })
+  }
+
+  const existingUser = await User.findOne({ username })
+  if (existingUser) {
+    return response.status(HTTP_STATUS.BAD_REQUEST).json({ error: 'username must be unique' })
+  }
+
+  const passwordHash = await bcrypt.hash(password, 10)
+
+  const user = new User({
+    username,
+    name,
+    passwordHash,
+  })
+
+  const savedUser = await user.save()
+
+  return response.status(HTTP_STATUS.CREATED).json(savedUser)
+})
+
+module.exports = usersRouter
