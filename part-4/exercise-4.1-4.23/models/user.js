5ee83d10424b8b78e0e8e1e2a9a6b9196ff93c2e
Jaime Pineda <jpin730@pm.me>
2026-02-19 16:42:25 -0600

    feat: :sparkles: add user model with schema and toJSON transformation

---
+++ part-4/exercise-4.1-4.23/models/user.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose')
+
+const config = require('../utils/config')
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    minlength: 3,
+  },
+  name: String,
+  passwordHash: String,
+  blogs: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Blog',
+    },
+  ],
+})
+
+userSchema.set('toJSON', {
+  transform: (_, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+    delete returnedObject.passwordHash
+  },
+})
+
+const collectionName = config.NODE_ENV === 'testing' ? 'testUsers' : 'users'
+
+const User = mongoose.model('User', userSchema, collectionName)
+
+module.exports = User
