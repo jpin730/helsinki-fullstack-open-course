c20c4d87371124704f36a31adefb94ed25f7cb02
Jaime Pineda <jpin730@pm.me>
2026-02-20 17:18:26 -0600

    feat: :passport_control: implement JWT authentication for blog creation

--- part-4/exercise-4.1-4.23/controllers/blogs.js
+++ part-4/exercise-4.1-4.23/controllers/blogs.js
@@ -1,7 +1,18 @@
 const blogsRouter = require('express').Router()
+const jwt = require('jsonwebtoken')
 
 const HTTP_STATUS = require('../consts/http-status')
+const config = require('../utils/config')
 const Blog = require('../models/blog')
+const User = require('../models/user')
+
+const getTokenFrom = (request) => {
+  const authorizationHeader = request.get('authorization')
+  if (authorizationHeader && authorizationHeader.startsWith('Bearer ')) {
+    return authorizationHeader.replace('Bearer ', '')
+  }
+  return null
+}
 
 blogsRouter.get('/', async (_, response) => {
   const blogs = await Blog.find({})
@@ -9,8 +20,34 @@ blogsRouter.get('/', async (_, response) => {
 })
 
 blogsRouter.post('/', async (request, response) => {
-  const blog = new Blog(request.body)
+  const token = getTokenFrom(request)
+  if (!token) {
+    return response.status(HTTP_STATUS.UNAUTHORIZED).json({ error: 'token missing' })
+  }
+
+  const tokenPayload = jwt.verify(token, config.JWT_SECRET)
+  const userId = tokenPayload.sub
+  if (!userId) {
+    return response.status(HTTP_STATUS.UNAUTHORIZED).json({ error: 'token invalid' })
+  }
+
+  const user = await User.findById(userId)
+  if (!user) {
+    return response.status(HTTP_STATUS.UNAUTHORIZED).json({ error: 'token invalid' })
+  }
+
+  const blog = new Blog({
+    title: request.body.title,
+    author: request.body.author,
+    url: request.body.url,
+    likes: request.body.likes,
+    user: user._id,
+  })
   const savedBlog = await blog.save()
+
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
+
   return response.status(HTTP_STATUS.CREATED).json(savedBlog)
 })
 
