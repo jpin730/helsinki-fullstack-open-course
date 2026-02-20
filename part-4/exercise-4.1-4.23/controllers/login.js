4f718adef279bda7983da78d4c653f00e291ea36
Jaime Pineda <jpin730@pm.me>
2026-02-19 18:31:51 -0600

    feat: :passport_control: implement login functionality with JWT authentication

---
+++ part-4/exercise-4.1-4.23/controllers/login.js
@@ -0,0 +1,41 @@
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
+
+const loginRouter = require('express').Router()
+
+const HTTP_STATUS = require('../consts/http-status')
+const config = require('../utils/config')
+const User = require('../models/user')
+
+loginRouter.post('/', async (request, response) => {
+  const { username, password } = request.body
+
+  const user = await User.findOne({ username })
+
+  if (!user) {
+    return response.status(HTTP_STATUS.UNAUTHORIZED).json({
+      error: 'invalid username or password',
+    })
+  }
+
+  const isPasswordCorrect = await bcrypt.compare(password, user.passwordHash)
+
+  if (!isPasswordCorrect) {
+    return response.status(HTTP_STATUS.UNAUTHORIZED).json({
+      error: 'invalid username or password',
+    })
+  }
+
+  const payload = {
+    username: user.username,
+    sub: user._id,
+  }
+
+  const token = jwt.sign(payload, config.JWT_SECRET)
+
+  const { name } = user
+
+  response.status(HTTP_STATUS.OK).send({ token, username, name })
+})
+
+module.exports = loginRouter
--- part-4/exercise-4.1-4.23/utils/config.js
+++ part-4/exercise-4.1-4.23/utils/config.js
@@ -2,7 +2,8 @@ const NODE_ENV = process.env.NODE_ENV
 
 process.loadEnvFile(NODE_ENV === 'testing' ? '.env.test.local' : '.env')
 
+const JWT_SECRET = process.env.JWT_SECRET
 const MONGODB_URI = process.env.MONGODB_URI
 const PORT = process.env.PORT
 
-module.exports = { MONGODB_URI, NODE_ENV, PORT }
+module.exports = { JWT_SECRET, MONGODB_URI, NODE_ENV, PORT }
