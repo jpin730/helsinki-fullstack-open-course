52ede5135abbce2b5b2a0484190cf3e36f4f1e2c
Jaime Pineda <jpin730@pm.me>
2026-02-23 15:19:44 -0600

    chore: :wrench: config eslint, prettier, husky and lint-staged

--- part-5/exercise-5.1-5.23/eslint.config.js
+++ part-5/exercise-5.1-5.23/eslint.config.js
@@ -1,9 +1,11 @@
 import js from '@eslint/js'
-import globals from 'globals'
+import prettier from 'eslint-plugin-prettier/recommended'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import globals from 'globals'
 
 export default [
+  prettier,
   { ignores: ['dist'] },
   {
     files: ['**/*.{js,jsx}'],
@@ -13,21 +15,18 @@ export default [
       parserOptions: {
         ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
-        sourceType: 'module'
-      }
+        sourceType: 'module',
+      },
     },
     plugins: {
       'react-hooks': reactHooks,
-      'react-refresh': reactRefresh
+      'react-refresh': reactRefresh,
     },
     rules: {
       ...js.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true }
-      ]
-    }
-  }
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    },
+  },
 ]
