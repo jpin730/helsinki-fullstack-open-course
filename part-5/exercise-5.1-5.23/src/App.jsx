aa1f79f22169f1f86aba9b65bf3c8b7999fb1513
Jaime Pineda <jpin730@pm.me>
2026-02-23 23:11:56 -0600

    feat: :sparkles: add Togglable component for conditional rendering of child components

--- part-5/exercise-5.1-5.23/src/App.jsx
+++ part-5/exercise-5.1-5.23/src/App.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react'
 import { Blog } from './components/Blog'
 import { BlogEditor } from './components/BlogEditor'
 import { LoginForm } from './components/LoginForm'
+import { Togglable } from './components/Toggable'
 import blogService from './services/blogs'
 
 const LOGGED_USER_KEY = 'loggedBlogAppUser'
@@ -12,14 +13,9 @@ export const App = () => {
   const [message, setMessage] = useState(null)
   const [blogs, setBlogs] = useState([])
 
-  useEffect(
-    function fetchBlogs() {
-      if (user) {
-        blogService.getAll().then((blogs) => setBlogs(blogs))
-      }
-    },
-    [user],
-  )
+  useEffect(function fetchBlogs() {
+    blogService.getAll().then((blogs) => setBlogs(blogs))
+  }, [])
 
   useEffect(function restoreUserSession() {
     const storedUser = localStorage.getItem(LOGGED_USER_KEY)
@@ -61,6 +57,8 @@ export const App = () => {
 
   return (
     <>
+      <h1>Blogs</h1>
+
       {message && (
         <p
           style={{
@@ -74,11 +72,14 @@ export const App = () => {
         </p>
       )}
 
-      {!user && <LoginForm onLogin={onLogin} onMessage={onMessage} />}
+      {!user && (
+        <Togglable buttonLabel="Login">
+          <LoginForm onLogin={onLogin} onMessage={onMessage} />
+        </Togglable>
+      )}
 
       {user && (
-        <div>
-          <h2>blogs</h2>
+        <>
           <p>
             <b>{user.name}</b> logged in
           </p>
@@ -86,15 +87,17 @@ export const App = () => {
             <button onClick={onLogout}>Logout</button>
           </p>
 
-          <BlogEditor onCreate={onCreate} onMessage={onMessage} token={user.token} />
-
-          <ol>
-            {blogs.map((blog) => (
-              <Blog key={blog.id} blog={blog} />
-            ))}
-          </ol>
-        </div>
+          <Togglable buttonLabel="Create new blog">
+            <BlogEditor onCreate={onCreate} onMessage={onMessage} token={user.token} />
+          </Togglable>
+        </>
       )}
+
+      <ol>
+        {blogs.map((blog) => (
+          <Blog key={blog.id} blog={blog} />
+        ))}
+      </ol>
     </>
   )
 }
---
+++ part-5/exercise-5.1-5.23/src/components/Toggable.jsx
@@ -0,0 +1,23 @@
+import { useImperativeHandle, useState } from 'react'
+
+export const Togglable = ({ children, ref, buttonLabel }) => {
+  const [isVisible, setVisible] = useState(false)
+
+  const toggleVisibility = () => setVisible(!isVisible)
+
+  useImperativeHandle(ref, () => {
+    return { toggleVisibility }
+  })
+
+  return (
+    <div>
+      <div style={{ display: isVisible ? 'none' : '' }}>
+        <button onClick={toggleVisibility}>{buttonLabel}</button>
+      </div>
+      <div style={{ display: isVisible ? '' : 'none' }}>
+        {children}
+        <button onClick={toggleVisibility}>Cancel</button>
+      </div>
+    </div>
+  )
+}
