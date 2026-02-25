cd8c1306618d5f53d14b94c86bf11b60909a9034
Jaime Pineda <jpin730@pm.me>
2026-02-25 17:46:33 -0600

    test: :white_check_mark: refactor Blog component tests for improved readability and structure

--- part-5/exercise-5.1-5.23/src/components/Blog.test.jsx
+++ part-5/exercise-5.1-5.23/src/components/Blog.test.jsx
@@ -3,45 +3,47 @@ import userEvent from '@testing-library/user-event'
 
 import { Blog } from './Blog'
 
-const MOCKED_BLOG = {
-  title: 'Test Blog',
-  author: 'John Doe',
-  url: 'https://example.com',
-  likes: 5,
-  user: {
-    name: 'Jane Smith',
-  },
-}
+describe('Blog', () => {
+  const MOCKED_BLOG = {
+    title: 'Test Blog',
+    author: 'John Doe',
+    url: 'https://example.com',
+    likes: 5,
+    user: {
+      name: 'Jane Smith',
+    },
+  }
 
-test('renders content by default state', () => {
-  // Arrange
-  const blog = MOCKED_BLOG
+  it('renders content by default state', () => {
+    // Arrange
+    const blog = MOCKED_BLOG
 
-  render(<Blog blog={blog} />)
+    render(<Blog blog={blog} />)
 
-  // Act
+    // Act
 
-  // Assert
-  const titleElement = screen.getByRole('heading', { level: 3 })
-  expect(titleElement).toHaveTextContent('Test Blog')
-  expect(titleElement).toHaveTextContent('John Doe')
+    // Assert
+    const titleElement = screen.getByRole('heading', { level: 3 })
+    expect(titleElement).toHaveTextContent('Test Blog')
+    expect(titleElement).toHaveTextContent('John Doe')
 
-  expect(screen.queryByText('https://example.com')).not.toBeInTheDocument()
-  expect(screen.queryByText(/likes/)).not.toBeInTheDocument()
-  expect(screen.queryByText('Jane Smith')).not.toBeInTheDocument()
-})
+    expect(screen.queryByText('https://example.com')).not.toBeInTheDocument()
+    expect(screen.queryByText(/likes/)).not.toBeInTheDocument()
+    expect(screen.queryByText('Jane Smith')).not.toBeInTheDocument()
+  })
 
-test('renders url, likes and user when view button is clicked', async () => {
-  // Arrange
-  const blog = MOCKED_BLOG
+  it('renders url, likes and user when view button is clicked', async () => {
+    // Arrange
+    const blog = MOCKED_BLOG
 
-  render(<Blog blog={blog} />)
+    render(<Blog blog={blog} />)
 
-  // Act
-  await userEvent.click(screen.getByRole('button', { name: 'view' }))
+    // Act
+    await userEvent.click(screen.getByRole('button', { name: 'view' }))
 
-  // Assert
-  expect(screen.queryByText('https://example.com')).toBeInTheDocument()
-  expect(screen.queryByText(/5 likes/)).toBeInTheDocument()
-  expect(screen.queryByText('Jane Smith')).toBeInTheDocument()
+    // Assert
+    expect(screen.queryByText('https://example.com')).toBeInTheDocument()
+    expect(screen.queryByText(/5 likes/)).toBeInTheDocument()
+    expect(screen.queryByText('Jane Smith')).toBeInTheDocument()
+  })
 })
