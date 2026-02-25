67b1cb85ab9bb5805bed17c66559d022967be8ad
Jaime Pineda <jpin730@pm.me>
2026-02-25 12:46:22 -0600

    feat: :sparkles: add update function to blog service for modifying existing blogs

--- part-5/exercise-5.1-5.23/src/services/blogs.js
+++ part-5/exercise-5.1-5.23/src/services/blogs.js
@@ -13,4 +13,9 @@ const create = async (blog, token) => {
   return response.data
 }
 
-export default { getAll, create }
+const update = async (id, blog) => {
+  const response = await axios.put(`${baseUrl}/${id}`, blog)
+  return response.data
+}
+
+export default { getAll, create, update }
