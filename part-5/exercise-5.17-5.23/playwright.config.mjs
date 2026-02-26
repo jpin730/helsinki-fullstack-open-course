17635bdbff54da908733e16f269cad97a449ca5c
Jaime Pineda <jpin730@pm.me>
2026-02-26 12:07:54 -0600

    refactor: :label: rename playwright with mjs extention

--- part-5/exercise-5.17-5.23/playwright.config.mjs
+++ part-5/exercise-5.17-5.23/playwright.config.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-import { defineConfig, devices } from '@playwright/test';
+import { defineConfig, devices } from '@playwright/test'
 
 export default defineConfig({
   testDir: './tests',
@@ -18,5 +18,4 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
   ],
-});
-
+})
