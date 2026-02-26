6af70e83fa706b5034c6a5dd0f42d76cd308f96c
Jaime Pineda <jpin730@pm.me>
2026-02-26 11:48:14 -0600

    test: :white_check_mark: add login test for user authentication in blog app

--- part-5/exercise-5.17-5.23/tests/blog_app.spec.js
+++ part-5/exercise-5.17-5.23/tests/blog_app.spec.js
@@ -7,4 +7,16 @@ describe('Blog app', () => {
     const locator = page.getByText('Blogs')
     await expect(locator).toBeVisible()
   })
+
+  test('user can log in', async ({ page }) => {
+    await page.goto('http://localhost:5173')
+
+    await page.getByRole('button', { name: 'login' }).click()
+    await page.getByRole('textbox', { name: 'Username' }).fill('root')
+    await page.getByRole('textbox', { name: 'Password' }).fill('sekret')
+    await page.getByRole('button', { name: 'login' }).click()
+
+    const locator = page.getByText('root logged in')
+    await expect(locator).toBeVisible()
+  })
 })
