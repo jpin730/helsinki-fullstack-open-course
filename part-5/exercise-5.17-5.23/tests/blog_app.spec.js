aac6553a8de2b133fb6284137c0bc4d9b15e561d
Jaime Pineda <jpin730@pm.me>
2026-02-26 12:42:00 -0600

    test: :white_check_mark: add test for creating a new blog when logged in

--- part-5/exercise-5.17-5.23/tests/blog_app.spec.js
+++ part-5/exercise-5.17-5.23/tests/blog_app.spec.js
@@ -19,4 +19,25 @@ describe('Blog app', () => {
     const locator = page.getByText('root logged in')
     await expect(locator).toBeVisible()
   })
+
+  describe('when logged in', () => {
+    beforeEach(async ({ page }) => {
+      await page.getByRole('button', { name: 'login' }).click()
+      await page.getByRole('textbox', { name: 'Username' }).fill('root')
+      await page.getByRole('textbox', { name: 'Password' }).fill('sekret')
+      await page.getByRole('button', { name: 'login' }).click()
+    })
+
+    test('a new blog can be created', async ({ page }) => {
+      await page.getByRole('button', { name: 'new blog' }).click()
+      await page.getByRole('textbox', { name: 'Title' }).fill('a blog created by playwright')
+      await page.getByRole('textbox', { name: 'Author' }).fill('playwright')
+      await page.getByRole('textbox', { name: 'URL' }).fill('http://example.com')
+      await page.getByRole('button', { name: 'create' }).click()
+
+      await expect(
+        page.getByText('Blog "a blog created by playwright" created successfully'),
+      ).toBeVisible()
+    })
+  })
 })
