fd2af715d4e57af782a2e6321acd5ad5d24da660
Jaime Pineda <jpin730@pm.me>
2026-02-27 17:16:51 -0600

    test: :white_check_mark: add and update tests for counter reducer

--- part-6/exercise-6.1-6.2/src/reducers/counterReducer.test.js
+++ part-6/exercise-6.1-6.2/src/reducers/counterReducer.test.js
@@ -1,13 +1,9 @@
 import deepFreeze from 'deep-freeze'
 import { describe, expect, test } from 'vitest'
-import counterReducer from './counterReducer'
+import { CounterActionTypes, counterReducer, INITIAL_STATE } from './counterReducer'
 
 describe('unicafe reducer', () => {
-  const initialState = {
-    good: 0,
-    ok: 0,
-    bad: 0,
-  }
+  const initialState = INITIAL_STATE
 
   test('should return a proper initial state when called with undefined state', () => {
     const action = {
@@ -20,7 +16,7 @@ describe('unicafe reducer', () => {
 
   test('good is incremented', () => {
     const action = {
-      type: 'GOOD',
+      type: CounterActionTypes.Good,
     }
     const state = initialState
 
@@ -31,5 +27,50 @@ describe('unicafe reducer', () => {
       ok: 0,
       bad: 0,
     })
+  })
+
+  test('ok is incremented', () => {
+    const action = {
+      type: CounterActionTypes.Ok,
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 1,
+      bad: 0,
+    })
+  })
+
+  test('bad is incremented', () => {
+    const action = {
+      type: CounterActionTypes.Bad,
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 0,
+      bad: 1,
+    })
+  })
+
+  test('state is reset to initial state', () => {
+    const action = {
+      type: CounterActionTypes.Reset,
+    }
+    const state = {
+      good: 5,
+      ok: 3,
+      bad: 2,
+    }
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual(initialState)
   })
 })
