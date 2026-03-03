314aa8a79260c3cb4ae5c26f5f4b81ec6bf2a655
Jaime Pineda <jpin730@pm.me>
2026-03-03 14:49:54 -0600

    feat: :sparkles: initialize anecdotes from server and refactor initial state

--- part-6/exercise-6.3-6.19/src/App.jsx
+++ part-6/exercise-6.3-6.19/src/App.jsx
@@ -1,9 +1,22 @@
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 import { AnecdoteForm } from './components/AnecdoteForm'
 import { AnecdoteList } from './components/AnecdoteList'
 import { Filter } from './components/Filter'
 import { Notification } from './components/Notification'
+import { setAnecdotes } from './reducers/anecdoteReducer'
+import anecdotesService from './services/anecdotes'
 
 export const App = () => {
+  const dispatch = useDispatch()
+
+  useEffect(
+    function initializeAnecdotes() {
+      anecdotesService.getAll().then((anecdotes) => dispatch(setAnecdotes(anecdotes)))
+    },
+    [dispatch],
+  )
+
   return (
     <main>
       <h2>Anecdotes</h2>
--- part-6/exercise-6.3-6.19/src/reducers/anecdoteReducer.js
+++ part-6/exercise-6.3-6.19/src/reducers/anecdoteReducer.js
@@ -1,30 +1,22 @@
 import { createSlice } from '@reduxjs/toolkit'
 
-const ANECDOTES_AT_START = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-]
-
 const getId = () => (100000 * Math.random()).toFixed(0)
 
 const asObject = (content) => ({ content, id: getId(), votes: 0 })
 
 const anecdoteSlice = createSlice({
   name: 'anecdotes',
-  initialState: ANECDOTES_AT_START.map(asObject),
+  initialState: [],
   reducers: {
     voteAnecdote: (state, action) => {
       const id = action.payload.id
       return state.map((a) => (a.id === id ? { ...a, votes: a.votes + 1 } : a))
     },
     createAnecdote: (state, action) => [...state, asObject(action.payload)],
+    setAnecdotes: (_, action) => action.payload,
   },
 })
 
-export const { voteAnecdote, createAnecdote } = anecdoteSlice.actions
+export const { voteAnecdote, createAnecdote, setAnecdotes } = anecdoteSlice.actions
 
 export const anecdoteReducer = anecdoteSlice.reducer
