049c85324dce4ae02456c902ce54bb7b175ab518
Jaime Pineda <jpin730@pm.me>
2026-03-03 11:22:29 -0600

    feat: :sparkles: add Filter component for filtering anecdotes

--- part-6/exercise-6.3-6.19/src/App.jsx
+++ part-6/exercise-6.3-6.19/src/App.jsx
@@ -1,11 +1,13 @@
 import { AnecdoteForm } from './components/AnecdoteForm'
 import { AnecdoteList } from './components/AnecdoteList'
+import { Filter } from './components/Filter'
 
 export const App = () => {
   return (
     <main>
       <h2>Anecdotes</h2>
       <hr />
+      <Filter />
       <AnecdoteList />
       <hr />
       <AnecdoteForm />
---
+++ part-6/exercise-6.3-6.19/src/components/Filter.jsx
@@ -0,0 +1,17 @@
+import { useDispatch } from 'react-redux'
+import { setFilter } from '../reducers/filterReducer'
+
+export const Filter = () => {
+  const dispatch = useDispatch()
+
+  const handleChange = (event) => {
+    const filter = event.target.value
+    dispatch(setFilter(filter.trim()))
+  }
+
+  return (
+    <label>
+      Filter <input onChange={handleChange} />
+    </label>
+  )
+}
