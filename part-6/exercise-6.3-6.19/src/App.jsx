f7afc6520c2291fd0301e00a2f0966caf088bf06
Jaime Pineda <jpin730@pm.me>
2026-03-03 15:25:47 -0600

    feat: :sparkles: refactor anecdote initialization to use async action creator

--- part-6/exercise-6.3-6.19/src/App.jsx
+++ part-6/exercise-6.3-6.19/src/App.jsx
@@ -1,21 +1,18 @@
 import { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
+
 import { AnecdoteForm } from './components/AnecdoteForm'
 import { AnecdoteList } from './components/AnecdoteList'
 import { Filter } from './components/Filter'
 import { Notification } from './components/Notification'
-import { setAnecdotes } from './reducers/anecdoteReducer'
-import anecdotesService from './services/anecdotes'
+import { initializeAnecdotes } from './reducers/anecdoteReducer'
 
 export const App = () => {
   const dispatch = useDispatch()
 
-  useEffect(
-    function initializeAnecdotes() {
-      anecdotesService.getAll().then((anecdotes) => dispatch(setAnecdotes(anecdotes)))
-    },
-    [dispatch],
-  )
+  useEffect(() => {
+    dispatch(initializeAnecdotes())
+  }, [dispatch])
 
   return (
     <main>
--- part-6/exercise-6.3-6.19/src/reducers/anecdoteReducer.js
+++ part-6/exercise-6.3-6.19/src/reducers/anecdoteReducer.js
@@ -1,5 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit'
 
+import anecdotesService from '../services/anecdotes'
+
 const anecdoteSlice = createSlice({
   name: 'anecdotes',
   initialState: [],
@@ -13,6 +15,15 @@ const anecdoteSlice = createSlice({
   },
 })
 
-export const { voteAnecdote, createAnecdote, setAnecdotes } = anecdoteSlice.actions
+const { setAnecdotes } = anecdoteSlice.actions
+
+export const initializeAnecdotes = () => {
+  return async (dispatch) => {
+    const anecdotes = await anecdotesService.getAll()
+    dispatch(setAnecdotes(anecdotes))
+  }
+}
+
+export const { voteAnecdote, createAnecdote } = anecdoteSlice.actions
 
 export const anecdoteReducer = anecdoteSlice.reducer
