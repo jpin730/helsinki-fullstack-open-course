94d3e2019abec3d70df089c440f5325b3bc7a592
Jaime Pineda <jpin730@pm.me>
2026-03-04 17:10:06 -0600

    feat: :wrench: setup tanstack react-query

--- part-6/exercise-6.20-6.24/src/main.jsx
+++ part-6/exercise-6.20-6.24/src/main.jsx
@@ -1,5 +1,18 @@
-import ReactDOM from 'react-dom/client'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { createRoot } from 'react-dom/client'
 
 import { App } from './App'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+})
+
+createRoot(document.getElementById('root')).render(
+  <QueryClientProvider client={queryClient}>
+    <App />
+  </QueryClientProvider>,
+)
