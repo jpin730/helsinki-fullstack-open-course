710caa1a2038419916154d4b1a10ac626c65ea48
Jaime Pineda <jpin730@pm.me>
2026-03-04 16:42:49 -0600

    style: :art: apply code formatting

--- part-6/exercise-6.20-6.24/eslint.config.js
+++ part-6/exercise-6.20-6.24/eslint.config.js
@@ -1,32 +1,32 @@
-import js from "@eslint/js";
-import prettier from "eslint-plugin-prettier/recommended";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
-import { defineConfig, globalIgnores } from "eslint/config";
-import globals from "globals";
+import js from '@eslint/js'
+import prettier from 'eslint-plugin-prettier/recommended'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import { defineConfig, globalIgnores } from 'eslint/config'
+import globals from 'globals'
 
 export default defineConfig([
   prettier,
-  globalIgnores(["dist"]),
+  globalIgnores(['dist']),
   {
-    files: ["**/*.{js,jsx}"],
+    files: ['**/*.{js,jsx}'],
     extends: [
       js.configs.recommended,
-      reactHooks.configs["recommended-latest"],
+      reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
     ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
       parserOptions: {
-        ecmaVersion: "latest",
+        ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
-        sourceType: "module",
+        sourceType: 'module',
       },
     },
     rules: {
-      "no-unused-vars": 0,
-      "no-constant-condition": 0,
+      'no-unused-vars': 0,
+      'no-constant-condition': 0,
     },
   },
-]);
+])
