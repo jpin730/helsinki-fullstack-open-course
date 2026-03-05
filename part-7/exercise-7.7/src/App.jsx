8b078e6efc52e47be4e8244a2dedc68248e772c2
Jaime Pineda <jpin730@pm.me>
2026-03-05 16:42:55 -0600

    refactor: :recycle: refactor app structure by creating separate components

--- part-7/exercise-7.7/src/App.jsx
+++ part-7/exercise-7.7/src/App.jsx
@@ -1,54 +1,14 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+import { useState } from 'react'
 
-const useField = (type) => {
-  const [value, setValue] = useState('')
+import { Country } from './components/Country'
+import { useCountry } from './hooks/useCountry'
+import { useField } from './hooks/useField'
 
-  const onChange = (event) => {
-    setValue(event.target.value)
-  }
-
-  return {
-    type,
-    value,
-    onChange
-  }
-}
-
-const useCountry = (name) => {
-  const [country, setCountry] = useState(null)
-
-  useEffect(() => {})
-
-  return country
-}
-
-const Country = ({ country }) => {
-  if (!country) {
-    return null
-  }
-
-  if (!country.found) {
-    return (
-      <div>
-        not found...
-      </div>
-    )
-  }
-
-  return (
-    <div>
-      <h3>{country.data.name} </h3>
-      <div>capital {country.data.capital} </div>
-      <div>population {country.data.population}</div> 
-      <img src={country.data.flag} height='100' alt={`flag of ${country.data.name}`}/>  
-    </div>
-  )
-}
-
-const App = () => {
-  const nameInput = useField('text')
+export const App = () => {
   const [name, setName] = useState('')
+
+  const nameInput = useField('text')
+
   const country = useCountry(name)
 
   const fetch = (e) => {
@@ -67,5 +27,3 @@ const App = () => {
     </div>
   )
 }
-
-export default App
