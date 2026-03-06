7b0df64cf8881fa066f140e45bd1353890f8ee77
Jaime Pineda <jpin730@pm.me>
2026-03-05 18:04:18 -0600

    feat: :sparkles: implement useCountry hook to fetch country data based on name

--- part-7/exercise-7.7/src/hooks/useCountry.js
+++ part-7/exercise-7.7/src/hooks/useCountry.js
@@ -1,9 +1,42 @@
 import { useEffect, useState } from 'react'
 
+const BASE_URL = 'https://studies.cs.helsinki.fi/restcountries/api'
+
+const numberFormatter = new Intl.NumberFormat('en-US', { notation: 'standard', useGrouping: true })
+const formatNumber = (num) => numberFormatter.format(num)
+
 export const useCountry = (name) => {
   const [country, setCountry] = useState(null)
 
-  useEffect(() => {})
+  useEffect(() => {
+    if (!name) return
+
+    const fetchCountry = async () => {
+      try {
+        const encodedName = encodeURIComponent(name.toLowerCase())
+        const response = await fetch(`${BASE_URL}/name/${encodedName}`)
+
+        if (!response.ok) {
+          throw new Error('Response not ok')
+        }
+
+        const data = await response.json()
+        setCountry({
+          found: true,
+          data: {
+            name: data.name.common,
+            capital: data.capital ? data.capital[0] : 'N/A',
+            population: formatNumber(data.population),
+            flag: data.flags.png,
+          },
+        })
+      } catch (error) {
+        setCountry({ found: false })
+      }
+    }
+
+    fetchCountry()
+  }, [name])
 
   return country
 }
