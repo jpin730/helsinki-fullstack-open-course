2b09780301cbf12e2bc7baac289c078e7a12e201
Jaime Pineda <jpin730@pm.me>
2026-03-06 21:59:15 -0600

    feat: :sparkles: implement useUser hook

--- part-7/exercise-7.9-7.21/src/App.jsx
+++ part-7/exercise-7.9-7.21/src/App.jsx
@@ -8,56 +8,30 @@ import { Notification } from './components/Notification'
 import { Togglable } from './components/Toggable'
 import { useNotification } from './hooks/useNotification'
 import { createBlog, deleteBlogById, initializeBlogs, likeBlog } from './reducers/blogReducer'
-import { login, logout, setUser } from './reducers/userReducer'
-
-const LOGGED_USER_KEY = 'loggedBlogAppUser'
+import { useUser } from './reducers/useUser'
 
 export const App = () => {
   const dispatch = useDispatch()
 
   const blogs = useSelector((state) => state.blogs)
-  const user = useSelector((state) => state.user)
 
   const blogFormTogglableRef = useRef()
   const blogFormRef = useRef()
 
   const { notify, notifyError } = useNotification()
+  const { user, login, logout } = useUser()
 
   useEffect(() => {
     dispatch(initializeBlogs())
   }, [dispatch])
 
-  useEffect(
-    function restoreUserSession() {
-      const storedUser = localStorage.getItem(LOGGED_USER_KEY)
-      if (storedUser) {
-        dispatch(setUser(JSON.parse(storedUser)))
-      }
-    },
-    [dispatch],
-  )
-
-  useEffect(
-    function syncUserToStorage() {
-      if (user) {
-        localStorage.setItem(LOGGED_USER_KEY, JSON.stringify(user))
-        return
-      }
-
-      localStorage.removeItem(LOGGED_USER_KEY)
-    },
-    [user],
-  )
-
   const handleLogin = async ({ username, password }) => {
     try {
-      await dispatch(login({ username, password }))
+      await login({ username, password })
     } catch (error) {
       notifyError(error.response?.data?.error ?? 'Login failed')
     }
   }
-
-  const handleLogout = () => dispatch(logout())
 
   const handleCreateBlog = async ({ title, author, url }) => {
     try {
@@ -115,7 +89,7 @@ export const App = () => {
           </p>
 
           <p>
-            <button onClick={handleLogout}>Logout</button>
+            <button onClick={logout}>Logout</button>
           </p>
 
           <Togglable label="Create new blog" ref={blogFormTogglableRef}>
---
+++ part-7/exercise-7.9-7.21/src/reducers/useUser.js
@@ -0,0 +1,45 @@
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+
+import { login as loginAction, logout as logoutAction, setUser } from './userReducer'
+
+const LOGGED_USER_KEY = 'loggedBlogAppUser'
+
+export const useUser = () => {
+  const dispatch = useDispatch()
+
+  const user = useSelector((state) => state.user)
+
+  useEffect(
+    function restoreUserSession() {
+      const storedUser = localStorage.getItem(LOGGED_USER_KEY)
+      if (storedUser) {
+        try {
+          dispatch(setUser(JSON.parse(storedUser)))
+        } catch {
+          localStorage.removeItem(LOGGED_USER_KEY)
+        }
+      }
+    },
+    [dispatch],
+  )
+
+  useEffect(
+    function syncUserToStorage() {
+      if (user) {
+        localStorage.setItem(LOGGED_USER_KEY, JSON.stringify(user))
+        return
+      }
+      if (localStorage.getItem(LOGGED_USER_KEY)) {
+        localStorage.removeItem(LOGGED_USER_KEY)
+      }
+    },
+    [user],
+  )
+
+  const login = ({ username, password }) => dispatch(loginAction({ username, password }))
+
+  const logout = () => dispatch(logoutAction())
+
+  return { user, login, logout }
+}
